649bf5afc6fc798aa47d49279803f4d6f34158b3
Les Aker <me@lesaker.org>
2017-07-07 16:18:00 -0400

    add prompt file

---
+++ prompt/main.go
@@ -0,0 +1,35 @@
+package prompt
+
+import (
+	"os"
+
+	"github.com/dixonwille/wmenu"
+)
+
+// PickFromList prompts for user input
+func PickFromList(message string, list []string, defaultOpt string) (string, error) {
+	c := make(chan string, 1)
+
+	// TODO: support picking by name
+	menu := wmenu.NewMenu(message)
+	menu.ChangeReaderWriter(os.Stdin, os.Stderr, os.Stderr)
+	menu.LoopOnInvalid()
+	menu.Action(func(opts []wmenu.Opt) error {
+		c <- opts[0].Value.(string)
+		return nil
+	})
+
+	for _, item := range list {
+		isDefault := false
+		if item == defaultOpt {
+			isDefault = true
+		}
+		menu.Option(item, item, isDefault, nil)
+	}
+
+	if err := menu.Run(); err != nil {
+		return "", err
+	}
+
+	return <-c, nil
+}
