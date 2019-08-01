0c7655534332e95c75913d67e5fa4432a3288adc
Les Aker <me@lesaker.org>
2019-08-01 09:59:54 +0100

    clean up file

--- travel/main.go
+++ travel/main.go
@@ -36,64 +36,144 @@ type Itinerary struct {
 	MfaPrompt   executors.MfaPrompt
 	Prompt      prompt.Func
 	Store       profiles.Store
-	pack        cartogram.Pack
-	account     cartogram.Account
-	path        []hop
-	creds       creds.Creds
+	pack        *cartogram.Pack
 }
 
 // Travel loads creds from a full set of parameters
 func (i *Itinerary) Travel() (creds.Creds, error) {
-	var c creds.Creds
+	return i.getCreds()
+}
 
+func (i *Itinerary) getStore() profiles.Store {
+	if i.Store == nil {
+		logger.InfoMsg("Using default profile store")
+		i.Store = profiles.NewDefaultStore()
+	}
+	return i.Store
+}
+
+func (i *Itinerary) getPrompt() prompt.Func {
 	if i.Prompt == nil {
 		logger.InfoMsg("Using default prompt")
 		i.Prompt = prompt.WithDefault
 	}
-
-	if err := i.loadPack(); err != nil {
-		return c, err
-	}
-	if err := i.loadAccount(); err != nil {
-		return c, err
-	}
-	if err := i.loadPath(); err != nil {
-		return c, err
-	}
-	if err := i.loadCreds(); err != nil {
-		return c, err
-	}
-	return i.creds, nil
+	return i.Prompt
 }
 
-func (i *Itinerary) loadPack() error {
-	i.pack = make(cartogram.Pack)
-	return i.pack.Load()
+func (i *Itinerary) getPack() (cartogram.Pack, error) {
+	if i.pack != nil {
+		return *i.pack, nil
+	}
+	i.pack = &cartogram.Pack{}
+	err := i.pack.Load()
+	return *i.pack, err
 }
 
-func (i *Itinerary) loadAccount() error {
+func (i *Itinerary) getAccount() (cartogram.Account, error) {
+	pack, err := i.getPack()
+	if err != nil {
+		return cartogram.Account{}, err
+	}
+	return pack.FindWithPrompt(i.Args, i.getPrompt())
+}
+
+func (i *Itinerary) getCreds() (creds.Creds, error) {
+	var c creds.Creds
 	var err error
-	i.account, err = i.pack.FindWithPrompt(i.Args, i.Prompt)
-	return err
-}
 
-func keys(input map[string]bool) []string {
-	list := []string{}
-	for k := range input {
-		list = append(list, k)
+	path, err := i.getPath()
+	if err != nil {
+		return c, err
 	}
-	return list
+
+	err = clearEnvironment()
+	if err != nil {
+		return c, err
+	}
+
+	profileHop, stack := path[0], path[1:]
+	store := i.getStore()
+	err = profiles.SetProfile(profileHop.Profile, store)
+	if err != nil {
+		return c, err
+	}
+	err = os.Setenv("AWS_DEFAULT_REGION", profileHop.Region)
+
+	last := len(stack) - 1
+	for index, thisHop := range stack {
+		c, err = i.executeHop(thisHop, c, index == last)
+		if err != nil {
+			break
+		}
+	}
+	return c, err
 }
 
-func (i *Itinerary) loadPath() error {
+func clearEnvironment() error {
+	for varName := range creds.Translations["envvar"] {
+		logger.InfoMsg(fmt.Sprintf("Unsetting env var: %s", varName))
+		err := os.Unsetenv(varName)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (i *Itinerary) executeHop(thisHop hop, c creds.Creds, isLast bool) (creds.Creds, error) {
+	var newCreds creds.Creds
+	logger.InfoMsg(fmt.Sprintf("Executing hop: %+v", thisHop))
+	a := executors.Assumption{}
+	if err := a.SetAccountID(thisHop.Account); err != nil {
+		return newCreds, err
+	}
+	if err := a.SetRoleName(thisHop.Role); err != nil {
+		return newCreds, err
+	}
+	if err := a.SetSessionName(i.SessionName); err != nil {
+		return newCreds, err
+	}
+	if err := a.SetLifetime(i.Lifetime); err != nil {
+		return newCreds, err
+	}
+	if isLast {
+		if err := a.SetPolicy(i.Policy); err != nil {
+			return newCreds, err
+		}
+	}
+	if thisHop.Mfa {
+		if err := a.SetMfa(true); err != nil {
+			return newCreds, err
+		}
+		if err := a.SetMfaSerial(i.MfaSerial); err != nil {
+			return newCreds, err
+		}
+		if err := a.SetMfaCode(i.MfaCode); err != nil {
+			return newCreds, err
+		}
+		if err := a.SetMfaPrompt(i.MfaPrompt); err != nil {
+			return newCreds, err
+		}
+	}
+	newCreds, err := a.ExecuteWithCreds(c)
+	newCreds.Region = thisHop.Region
+	return newCreds, err
+}
+
+func (i *Itinerary) getPath() ([]hop, error) {
 	var paths [][]hop
 	mapProfiles := make(map[string]bool)
 	mapRoles := make(map[string]bool)
 
-	for _, r := range i.account.Roles {
-		p, err := i.tracePath(i.account, r)
+	account, err := i.getAccount()
+	if err != nil {
+		return []hop{}, err
+	}
+
+	for _, r := range account.Roles {
+		p, err := i.tracePath(account, r)
 		if err != nil {
-			return err
+			return []hop{}, err
 		}
 		for _, item := range p {
 			paths = append(paths, item)
@@ -102,9 +182,9 @@ func (i *Itinerary) loadPath() error {
 	}
 
 	allRoles := keys(mapRoles)
-	role, err := i.Prompt.Filtered(i.RoleName, allRoles, "Desired target role:")
+	role, err := i.getPrompt().Filtered(i.RoleName, allRoles, "Desired target role:")
 	if err != nil {
-		return err
+		return []hop{}, err
 	}
 	hopsWithMatchingRoles := [][]hop{}
 	for _, item := range paths {
@@ -115,9 +195,9 @@ func (i *Itinerary) loadPath() error {
 	}
 
 	allProfiles := keys(mapProfiles)
-	profile, err := i.Prompt.Filtered(i.ProfileName, allProfiles, "Desired target profile:")
+	profile, err := i.getPrompt().Filtered(i.ProfileName, allProfiles, "Desired target profile:")
 	if err != nil {
-		return err
+		return []hop{}, err
 	}
 	hopsWithMatchingProfiles := [][]hop{}
 	for _, item := range hopsWithMatchingRoles {
@@ -129,39 +209,18 @@ func (i *Itinerary) loadPath() error {
 	if len(hopsWithMatchingProfiles) > 1 {
 		logger.InfoMsg("Multiple valid paths detected. Selecting the first option")
 	}
-	i.path = hopsWithMatchingProfiles[0]
-
-	return nil
+	return hopsWithMatchingProfiles[0], nil
 }
 
 func (i *Itinerary) tracePath(acc cartogram.Account, role cartogram.Role) ([][]hop, error) {
 	var srcHops [][]hop
 
-	logger.DebugMsg(fmt.Sprintf("Tracing from %s / %s", acc.Account, role.Name))
-
 	for _, item := range role.Sources {
 		pathMatch := cartogram.AccountRegex.FindStringSubmatch(item.Path)
 		if len(pathMatch) == 3 {
-			srcAccID := pathMatch[1]
-			ok, srcAcc := i.pack.Lookup(srcAccID)
-			if !ok {
-				logger.DebugMsg(fmt.Sprintf("Found dead end due to missing account: %s", srcAccID))
-				continue
-			}
-			srcRoleName := pathMatch[2]
-			ok, srcRole := srcAcc.Roles.Lookup(srcRoleName)
-			if !ok {
-				logger.DebugMsg(fmt.Sprintf(
-					"Found dead end due to missing role: %s/%s", srcAccID, srcRoleName,
-				))
-				continue
-			}
-			newPaths, err := i.tracePath(srcAcc, srcRole)
+			err := i.testSourcePath(pathMatch[1], pathMatch[2], &srcHops)
 			if err != nil {
-				return srcHops, err
-			}
-			for _, np := range newPaths {
-				srcHops = append(srcHops, np)
+				return [][]hop{}, err
 			}
 		} else {
 			srcHops = append(srcHops, []hop{{Profile: item.Path}})
@@ -181,75 +240,33 @@ func (i *Itinerary) tracePath(acc cartogram.Account, role cartogram.Role) ([][]h
 	return srcHops, nil
 }
 
-func (i *Itinerary) getStore() profiles.Store {
-	if i.Store == nil {
-		i.Store = profiles.NewDefaultStore()
+func (i *Itinerary) testSourcePath(srcAccID, srcRoleName string, srcHops *[][]hop) error {
+	ok, srcAcc := i.pack.Lookup(srcAccID)
+	if !ok {
+		logger.DebugMsg(fmt.Sprintf("Found dead end due to missing account: %s", srcAccID))
+		return nil
 	}
-	return i.Store
-}
-
-func (i *Itinerary) loadCreds() error {
-	var c creds.Creds
-	var err error
-
-	profileHop, stack := i.path[0], i.path[1:]
-	for varName := range creds.Translations["envvar"] {
-		logger.InfoMsg(fmt.Sprintf("Unsetting env var: %s", varName))
-		err = os.Unsetenv(varName)
-		if err != nil {
-			return err
-		}
+	ok, srcRole := srcAcc.Roles.Lookup(srcRoleName)
+	if !ok {
+		logger.DebugMsg(fmt.Sprintf(
+			"Found dead end due to missing role: %s/%s", srcAccID, srcRoleName,
+		))
+		return nil
 	}
-	store := i.getStore()
-	err = profiles.SetProfile(profileHop.Profile, store)
+	newPaths, err := i.tracePath(srcAcc, srcRole)
 	if err != nil {
 		return err
 	}
-	err = os.Setenv("AWS_DEFAULT_REGION", profileHop.Region)
-
-	last := len(stack) - 1
-	for index, thisHop := range stack {
-		logger.InfoMsg(fmt.Sprintf("Executing hop: %+v", thisHop))
-		a := executors.Assumption{}
-		if err := a.SetAccountID(thisHop.Account); err != nil {
-			return err
-		}
-		if err := a.SetRoleName(thisHop.Role); err != nil {
-			return err
-		}
-		if err := a.SetSessionName(i.SessionName); err != nil {
-			return err
-		}
-		if i.Lifetime != 0 {
-			if err := a.SetLifetime(i.Lifetime); err != nil {
-				return err
-			}
-		}
-		if index == last {
-			if err := a.SetPolicy(i.Policy); err != nil {
-				return err
-			}
-		}
-		if thisHop.Mfa {
-			if err := a.SetMfa(true); err != nil {
-				return err
-			}
-			if err := a.SetMfaSerial(i.MfaSerial); err != nil {
-				return err
-			}
-			if err := a.SetMfaCode(i.MfaCode); err != nil {
-				return err
-			}
-			if err := a.SetMfaPrompt(i.MfaPrompt); err != nil {
-				return err
-			}
-		}
-		c, err = a.ExecuteWithCreds(c)
-		c.Region = thisHop.Region
-		if err != nil {
-			return err
-		}
+	for _, np := range newPaths {
+		*srcHops = append(*srcHops, np)
 	}
-	i.creds = c
 	return nil
+}
+
+func keys(input map[string]bool) []string {
+	list := []string{}
+	for k := range input {
+		list = append(list, k)
+	}
+	return list
 }
